86f7ed182843419611369f4cd2db0548ce841a72
Dustin Lee <dustidl1@uci.edu>
2025-03-15 01:39:49 +0000

    d

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -29,7 +29,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 }
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
     vector<int> path;
-    for (int i = destination; i != -1; at = previous[i]) {
+    for (int i = destination; i != -1; i = previous[i]) {
         path.push_back(i);
     }
     reverse(path.begin(), path.end());
